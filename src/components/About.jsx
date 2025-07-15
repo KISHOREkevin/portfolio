77a640b02852aea7381c62274cfd96afd01406cd
KISHORE kevin <kishorekevin3289@gmail.com>
2025-07-15 09:26:23 +0530

    Project Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <h1 className='text-6xl font-bold text-ctp-rosewater'>I'm Kishore</h1>
             <h3 className='text-3xl font-bold text-ctp-rosewater'>Fullstack developer</h3>
             <p className='lg:text-xl text-[15px] mt-2 '>Linux enthusiast | I use Arch Linux <i className="devicon-archlinux-plain colored"></i> btw</p>
-        <a href="/assets/resume.pdf" download><button className='bg-ctp-blue text-xl text-black p-2 mt-2 rounded-md w-full'>My Resume</button></a>  
+        <a href="/assets/resume.pdf" download><button className='bg-ctp-blue text-xl text-black p-2 mt-2 rounded-md w-full'>Download Resume</button></a>  
       </div>
     </div>
     </div>
